a776adea87c762333a3f0c7c075a98008dee6e54
Anthony Wong <anthonywong@ucsb.edu>
2025-10-31 12:12:23 -0700

    created fixtures for UCSBMenuItem

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItem.js
@@ -0,0 +1,30 @@
+const ucsbDiningCommonsMenuItem = {
+  oneUCSBDiningCommonsMenuItem: {
+    id: 1,
+    diningCommonsCode: "DLG",
+    name: "Chicken Tenders",
+    station: "Grill",
+  },
+  threeUCSBDiningCommonsMenuItem: [
+    {
+      id: 1,
+      diningCommonsCode: "DLG",
+      name: "Chicken Tenders",
+      station: "Grill",
+    },
+    {
+      id: 2,
+      diningCommonsCode: "ORTEGA",
+      name: "Rice",
+      station: "N/A",
+    },
+    {
+      id: 3,
+      diningCommonsCode: "CARILLO",
+      name: "Cookies",
+      station: "Desert",
+    },
+  ],
+};
+
+export { ucsbDiningCommonsMenuItem };
