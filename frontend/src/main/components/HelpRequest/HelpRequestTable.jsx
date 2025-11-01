e5224e1f4b9b6dc10772cd5defa3372cdff1a634
Ajay Paismanathan <ajaypaismanathan@ucsb.edu>
2025-11-01 13:49:20 -0700

    ap - Added table component & tests for HelpRequest

---
+++ frontend/src/main/components/HelpRequest/HelpRequestTable.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import OurTable, { ButtonColumn } from "main/components/OurTable";
+
+import { useBackendMutation } from "main/utils/useBackend";
+import {
+  cellToAxiosParamsDelete,
+  onDeleteSuccess,
+} from "main/utils/HelpRequestUtils";
+import { useNavigate } from "react-router";
+import { hasRole } from "main/utils/useCurrentUser";
+
+export default function HelpRequestTable({ helpRequests, currentUser }) {
+  const navigate = useNavigate();
+
+  const editCallback = (cell) => {
+    navigate(`/HelpRequest/edit/${cell.row.original.id}`);
+  };
+
+  // Stryker disable all : hard to test for query caching
+
+  const deleteMutation = useBackendMutation(
+    cellToAxiosParamsDelete,
+    { onSuccess: onDeleteSuccess },
+    ["/api/HelpRequest/all"],
+  );
+  // Stryker restore all
+
+  // Stryker disable next-line all : TODO try to make a good test for this
+  const deleteCallback = async (cell) => {
+    deleteMutation.mutate(cell);
+  };
+
+  const columns = [
+    {
+      header: "id",
+      accessorKey: "id", // accessor is the "key" in the data
+    },
+    {
+      header: "Requester Email",
+      accessorKey: "requesterEmail",
+    },
+    {
+      header: "Team Id",
+      accessorKey: "teamId",
+    },
+    {
+      header: "Table Or Breakout Room",
+      accessorKey: "tableOrBreakoutRoom",
+    },
+    {
+      header: "Request Time (ISO Format)",
+      accessorKey: "requestTime",
+    },
+    {
+      header: "Explanation",
+      accessorKey: "explanation",
+    },
+    {
+      header: "Solved",
+      accessorKey: "solved",
+    },
+  ];
+
+  if (hasRole(currentUser, "ROLE_ADMIN")) {
+    columns.push(
+      ButtonColumn("Edit", "primary", editCallback, "HelpRequestTable"),
+    );
+    columns.push(
+      ButtonColumn("Delete", "danger", deleteCallback, "HelpRequestTable"),
+    );
+  }
+
+  return <OurTable data={helpRequests} columns={columns} testid={"HelpRequestTable"} />;
+}
