49b0131de26db21626245942c7450bdebdf14eaf
davey-j1 <dbanan@ucsb.edu>
2025-11-04 18:42:39 -0800

    Remove UCSBOrganization link from AppNavbar

--- frontend/src/main/components/Nav/AppNavbar.jsx
+++ frontend/src/main/components/Nav/AppNavbar.jsx
@@ -102,9 +102,6 @@ export default function AppNavbar({
                   <Nav.Link as={Link} to="/placeholder">
                     Placeholder
                   </Nav.Link>
-                  <Nav.Link as={Link} to="/ucsborganization">
-                    UCSBOrganization
-                  </Nav.Link>
                 </>
               ) : (
                 <></>
