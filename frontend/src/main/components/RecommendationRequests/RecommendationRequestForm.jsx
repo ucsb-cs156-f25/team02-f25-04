19ff22bfae39e19b088c946b41a16b7ec681bf89
Max Chang <maxwellchang@ucsb.edu>
2025-11-06 18:09:50 -0800

    added testid to request form, finished up e2e tests

--- frontend/src/main/components/RecommendationRequests/RecommendationRequestForm.jsx
+++ frontend/src/main/components/RecommendationRequests/RecommendationRequestForm.jsx
@@ -159,7 +159,7 @@ function RecommendationRequestForm({
         />
       </Form.Group>
 
-      <Button type="submit">{buttonLabel}</Button>
+      <Button type="submit" data-testid={testIdPrefix + "-submit"}>{buttonLabel}</Button>
       <Button
         variant="Secondary"
         onClick={() => navigate(-1)}
